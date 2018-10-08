106b599b4742196c23010e375752098b141e3b38
andy <andy870622@gmail.com>
2018-10-08 17:21:50 +0800

    1

---
+++ homework/homework/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  homework
+//
+//  Created by User15 on 2018/10/7.
+//  Copyright © 2018 User15. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
